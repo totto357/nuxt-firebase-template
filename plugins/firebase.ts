36a1bb562932aba08b46540a900a34dd2e00cb0a
totto357 <totto357@gmail.com>
2020-04-02 21:06:01 +0900

    Create firebase.ts

---
+++ plugins/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from "firebase"
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export const firebaseAuth = firebase.auth()
+export const firestore = firebase.firestore()
+export const storage = firebase.storage()
